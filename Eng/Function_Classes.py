942f0af7c9fa8f33efad4fc039e3b1fd119cc1ba
AlephInfi <dyrelicgd@gmail.com>
2026-03-24 14:03:40 -0500

    Add some constants to establish structure

---
+++ Eng/Function_Classes.py
@@ -0,0 +1,41 @@
+# import libraries
+import numpy as np
+import scipy
+import scipy.linalg as spla
+
+# you can define classes with their own variables if you're
+# instantiating a lot of unique things in a function, i.e.
+# custom vector math, vertices in 3d space, etc.
+class FuncHelperEx:
+    __private_variable = 0
+    public_variable = 1
+
+    def __init__(self, num):
+        self.__private_variable = num
+
+    def getprivatevariable(self):
+        return self.__private_variable
+
+# WRITE CUSTOM CLASSES BELOW #
+
+class body:
+    __u = np.array([0,0,0])
+    __v = np.array([0, 0, 0])
+    __a = np.array([0, 0, 0])
+
+    # assume vectors 0 if not specified or unknown
+    def __init__(self,
+                 u: np.ndarray | None = None,
+                 v: np.ndarray | None = None,
+                 a: np.ndarray | None = None):
+        if u.shape != (3,) or v.shape != (3,) or a.shape != (3,):
+            raise ValueError("u, v, and a must be 3-element vectors")
+
+        # create fresh defaults if None
+        u = np.zeros(3) if u is None else u
+        v = np.zeros(3) if v is None else v
+        a = np.zeros(3) if a is None else a
+
+        self.__u = u
+        self.__v = v
+        self.__a = a
---
+++ Eng/n_body_calc_defs.py
@@ -0,0 +1,18 @@
+import numpy as np
+import Function_Classes
+
+const_G = 6.67430 * 10**(-11) # m^3/(kg s^2)
+const_m_Earth = 5.972 * 10**24 # kg
+const_m_Moon = 7.348 * 10**22 # kg
+const_m_sat = 1000 # kg
+const_m_asteroid = 1 * 10**10 # kg
+const_r_Moon = 3.844 * 10**8 # m
+const_r_Earth = 6371 # km
+r_sat =
+
+def find_v_sqrt_GM_r(M, r):
+    return np.sqrt(const_G * M / r)
+
+v_moon = find_v_sqrt_GM_r(const_m_Earth, const_r_Moon) # ~ 1.02 * 10^3
+
+v = 1
